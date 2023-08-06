c966d1f68d2bd0157c61abe767398e4112716d9e
Ilyar <761285+ilyar@users.noreply.github.com>
2023-08-06 03:35:01 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.scss'
+import githubLogo from './assets/github.svg'
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src={ logo } className="logo" alt="Ever Guild"/>
+      </div>
+      <h2>Ever Guild</h2>
+      <div className="card">
+        <a className="center" href="https://github.com/ever-guild">
+          <img src={ githubLogo } className="logo github" alt="Ever Guild"/>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
