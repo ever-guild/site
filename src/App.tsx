a0a6815a89b02615d7e553df6897b47d129e7b5b
Ilyar <ilyar.software@gmail.com>
2026-05-24 19:10:58 +0200

    fix: align social links layout

--- src/App.tsx
+++ src/App.tsx
@@ -22,14 +22,14 @@ function App() {
   return (
     <div className="App">
       <div>
-        <img src={ logo } className="logo" alt="Ever Guild"/>
+        <img src={ logo } className="logo brand-logo" alt="Ever Guild"/>
       </div>
       <h2>Ever Guild</h2>
       <div className="card">
-        <a className="center" href="https://github.com/ever-guild" aria-label="Ever Guild on GitHub">
-          <img src={ githubLogo } className="logo github" alt="Ever Guild"/>
-        </a>
         <div className="social-links">
+          <a href="https://github.com/ever-guild" aria-label="Ever Guild on GitHub">
+            <img src={ githubLogo } className="github-icon" alt=""/>
+          </a>
           <a href="https://linkedin.com/company/ever-guild/" aria-label="Ever Guild on LinkedIn">
             <LinkedInIcon />
           </a>
