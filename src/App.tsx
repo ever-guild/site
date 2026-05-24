c12e88fac1a0b1973a258d101587687a298bf872
Ilyar <ilyar.software@gmail.com>
2026-05-24 18:55:22 +0200

    fix: update action runtimes and social links

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,13 @@ function App() {
       </div>
       <h2>Ever Guild</h2>
       <div className="card">
-        <a className="center" href="https://github.com/ever-guild">
+        <a className="center" href="https://github.com/ever-guild" aria-label="Ever Guild on GitHub">
           <img src={ githubLogo } className="logo github" alt="Ever Guild"/>
         </a>
+        <div className="social-links">
+          <a href="https://linkedin.com/company/ever-guild/">LinkedIn</a>
+          <a href="https://x.com/ever_guild_net">X</a>
+        </div>
       </div>
     </div>
   )
